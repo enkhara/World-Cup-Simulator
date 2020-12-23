5551e5c7afa27c3b73cd9183c77eeff701f20ec5
Georgina Torralba <georgina.torralba@gmail.com>
2020-12-23 18:08:31 +0100

    Añado archivo teams.js con el objeto team y el array teamNames

---
+++ teams.js
@@ -0,0 +1,25 @@
+
+
+export const team = {
+    name: null,
+    pointsPerWin: 0,
+    pointsPerDraw: 0,
+    pointsPerLose: 0,
+    goalsFor: 0,
+    goalsAgainst: 0
+};
+
+
+export const teamsNames = [
+    'Espana',  'Australia', 'Chile', 'Paises Bajos',
+    'Brasil', 'Croacia', 'Mexico', 'Camerún',
+    'Colombia', 'Grecia',  'Costa de Marfil', 'Japón',
+    'Uruguay', 'Costa Rica', 'Inglaterra','Italia', 
+    'Suiza', 'Ecuador','Francia', 'Honduras',
+    'Argentina', 'Bosnia & H', 'Irán', 'Nigeria',
+    'Alemania', 'Portugal', 'Ghana', 'Estados Unidos', 
+    'Holanda', 'Algeria', 'Rusia', 'Korea'
+
+];
+
+console.log('hay', teamsNames.length, ' paises')
