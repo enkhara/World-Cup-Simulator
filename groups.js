88e81db8e0d366c5381c49d54afb85008e2682b3
Georgina Torralba <georgina.torralba@gmail.com>
2020-12-25 05:00:28 +0100

    Añado función shuffle

---
+++ groups.js
@@ -0,0 +1,17 @@
+import {team, teamsNames} from './teams.js'
+
+
+Array.prototype.shuffle = function()
+{
+    var i = this.length;
+	while (i)
+	{
+        var j = Math.floor(Math.random() * i);
+		var t = this[--i];
+		this[i] = this[j];
+		this[j] = t;
+	}
+	return this;
+}
+
+
