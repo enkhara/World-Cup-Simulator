6c03cd78e04980da9ce9b89123961b98ad7baca4
Georgina Torralba <georgina.torralba@gmail.com>
2020-12-29 13:01:51 +0100

    Archivo teams.js con objeto team y array teamsNames con nombres de los participantes de la WorldCup

--- teams.js
+++ teams.js
@@ -1,15 +1,15 @@
 
+//creem objecte equip
+export const team = {
+    name: null,
+    pointsPerWin: 0,
+    pointsPerDraw: 0,
+    pointsPerLose: 0,
+    goalsFor: 0,
+    goalsAgainst: 0
+};
 
-// export const team = {
-//     name: null,
-//     pointsPerWin: 0,
-//     pointsPerDraw: 0,
-//     pointsPerLose: 0,
-//     goalsFor: 0,
-//     goalsAgainst: 0
-// };
-
-
+// noms dels equips que participen a la lliga
 export const teamsNames = [
     'Espana',  'Australia', 'Chile', 'Paises Bajos',
     'Brasil', 'Croacia', 'Mexico', 'Camerún',
@@ -19,7 +19,5 @@ export const teamsNames = [
     'Argentina', 'Bosnia & H', 'Irán', 'Nigeria',
     'Alemania', 'Portugal', 'Ghana', 'Estados Unidos', 
     'Holanda', 'Algeria', 'Rusia', 'Korea'
-
 ];
 
-console.log('hay', teamsNames.length, ' paises')
