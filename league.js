58389f43cea6850124e5388728e252c73746fad3
Georgina Torralba <georgina.torralba@gmail.com>
2020-12-29 13:02:43 +0100

    Añado el objeto match para los partidos y la funcion en construccion de allAgainstAll

--- league.js
+++ league.js
@@ -1,37 +1,77 @@
+import {team, teamsNames} from './teams.js'
 
+const match ={
+    homeTeam: null,
+    awayTeam: null,
+    goalsHome: 0,
+    goalsAway: 0
+}
 
-// const numberOfTeamsPerGroup = 4;
+//omplim l'array lliga amb l'objecte team amb la informació 
+export function leagueTeamsCreator(leagueTeams) {
+    //const leagueTeams =[];
+    teamsNames.forEach(teamName =>{
+        const teamPerLeague = Object.assign({}, team);
+        teamPerLeague.name = teamName;
+        leagueTeams.push(teamPerLeague)
+    })
+    return (leagueTeams);
+}
+// 3 jornades i 2 partits/ jornada
+function allAgainstAll(group){
+    const maxRow = group.teams.length - 2;
+    const maxColumn = group.teams.length / 2;
 
-// const league = {
-//     howManyLeagues: 
-// }
+    
+    const matchesPerDay = [];
+    
+    const lasTeamNamePosition = group.teams.length - 1;
+    for( let row = 0; row < maxRow; row++ ) {
+        let teamNamePosition = 0;
+        for (let column = 0; column < maxColumn; column++){
+            const matchSchedule = Object.assign({}, match);
+            if (teamNamePosition >= lasTeamNamePosition){
+                teamNamePosition = 0;
+            }
+            matchSchedule.homeTeam = group.teams[teamNamePosition];
+            matchSchedule.awayTeam = group.teams[lasTeamNamePosition];
+            //console.log(matchSchedule)
+            //matchesPerDay[row][column]= matchSchedule
+            matchesPerDay.push(matchSchedule)
+            teamNamePosition++;
 
-// function howManyLeagues (numberOfTeams){
-
-// }
-
-export class League {
-    constructor(name, rounds, teams= []){
-        this.name = name;
-        this.rounds = rounds;
-        this.setupTeams(teams);
+        }group.schedule.push(matchesPerDay);
     }
+    //console.log('ALL against ALL', group.schedule)
+}
+function allAgainstAllAlgorithm(group){
+    group.schedule = []
+    
+    for (let i = 0; i < group.teams.length-1; i++){
+        const matchDay = [];
+        for( let j = 0; j <group.teams.length/2; j++){
+            const matches = ['localTeam', 'AwayTeam'];
+            matchDay.push(matches);
 
-    setupTeams(teams){
-        this.teams = []
-
-        for (const team of teams){
-            this.team.push(createTeam(team));
-        }
-    }
-    createTeam(team){
-        return {
-            name: team,
-            pointsPerWin: 0,
-            pointsPerDraw: 0,
-            pointsPerLose: 0,
-            goalsFor: 0,
-            goalsAgainst: 0
-        }
+        
+        }group.schedule.push(matchDay)
     }
 }
+
+// creen la planificació de partits per cada grup
+export function scheduleMatches(groups){ 
+    groups.forEach(group =>{
+        
+        allAgainstAllAlgorithm(group);
+
+        console.log('Grupo',group.name, '\nJornada 1\n',group.schedule[0], '\nJornada 2\n',group.schedule[1], '\nJornada 3\n',group.schedule[2])
+    })    
+}
+
+// for (let i = 0; i < 4; i++ ){
+//     const teamsPerGroup = Object.assign({}, team)
+//     teamsPerGroup.name = teamsNames[i]; 
+//     groupA.push(teamsPerGroup)
+// }
+
+
