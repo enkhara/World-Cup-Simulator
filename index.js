79ac649205ecde73d6493772d09140925ce0a0f1
Georgina Torralba <georgina.torralba@gmail.com>
2020-12-29 12:58:54 +0100

    Eliminio de index.js las líneas de código comentadas

--- index.js
+++ index.js
@@ -1,57 +1,24 @@
 //exporto objecte team
 import {team, teamsNames} from './teams.js'
-//import teamsNames from './teams.js'
-//import League from './league.js'
+import creatorGroups from './groups.js'
+import {leagueTeamsCreator, scheduleMatches} from './league.js'
+import {showGroupsAndTeams} from './consoleLog.js'
 
-//creo la lliga array d'objectes i afegeixo l'objete team
-
-
-/*
-const numberOfTeams = teamsNames.length;
-
+const groups = [];
+const leagueTeams = [];
 function start() {
-    //fase de grups
-    howManyLeagues(numberOfTeams);
-    //leguesCreator(howManyLegues())
-        //definir número de lligues que hi haurà (equips/4)
-        //crear les lligues i omplirles amb els equips shuffle i slide
-        //algoritme de tots contra tots 
-        //jugar els partits
-        //omplir les estadístiques find
-        //classificació sort
-    //mostrar resultats per pantalla
-        //pintar per pantalla classificació
-    //fase eliminatòria
-        //quedarse amb els dos primers de cada lliga
-        // de dos en dos lligues enfrontar el primer amb el segon 
-        //crear dues fases eliminatories per que no coincideixin els de una mateixa lliga
-    //gestionar grups de 2 en dos 1r contra segon
 
-    //mostrar resultats per pantalla
+    leagueTeamsCreator(leagueTeams);
+
+    //FASE DE GRUPS
+    
+    creatorGroups(groups);
+    scheduleMatches(groups);
+    //groups.forEach(group => console.log(group.schedule))
+    //console.log(groups);
+    
+    //showGroupsAndTeams(groups);
+
 }
 
 start();
-const groups = 8; 
-function leaguesNames(groups) {
-    const leagueNames = [];
-    for (let i = 65; i < groups + 65; i ++){
-        leagueNames.push(String.fromCharCode(i));
-    }
-    console.log (`aux ${leagueNames} ` );
-    return leagueNames;
-}
-const names = leaguesNames(groups);
-*/
-
-
-
-const groupA = [];
-
-for (let i = 0; i < 4; i++ ){
-    const teamsPerGroup = Object.assign({}, team)
-    teamsPerGroup.name = teamsNames[i]; 
-    groupA.push(teamsPerGroup)
-}
-
-console.table(groupA)
-
