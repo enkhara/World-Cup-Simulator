9bc769b3ce47054d3004f0349764fb56958b35f5
Georgina Torralba <georgina.torralba@gmail.com>
2021-01-05 05:01:13 +0100

    Creo funciones dividingLine, modifico la función showTitleWorldCupStageBegins y termino la función showMatchesAndStandings

--- consoleLog.js
+++ consoleLog.js
@@ -1,9 +1,15 @@
 
+function dividingLine(symbol, repeat = 20){
+    console.log(symbol.repeat(repeat));
+}
+
 export function showGroupsAndTeams(groups){
-    console.log('\nGrupos y equipos\n===============================');
+    console.log('\nGrupos y equipos');
+    dividingLine('=');
     
     groups.forEach(group =>{
-        console.log(`\nGroup ${group.name}\n---------------------`)
+        console.log(`\nGroup ${group.name}`)
+        dividingLine('-')
         group.teams.forEach(team  =>{
             console.log(team)
         });
@@ -17,6 +23,36 @@ export function showGroupsAndTeams(groups){
     });
 }
 
-export function showMatchesPerDay(groups,index){
+export function showTitleWorldCupStageBegins(title){
+    const titleLenght = title.length
+    console.log('\n')
+    dividingLine('=', titleLenght + 14);
+    console.log('='.repeat(6), title,'='.repeat(6))
+    dividingLine('=', titleLenght + 14);
+}
 
+export function showMatchesAndStandings(group,  leagueTeams, jornada){
+
+    const index = group.name.charCodeAt(0);
+    const indexSliceInit = index - 65;
+    const indexSliceEnd = indexSliceInit + 4;
+    //console.log(matchesPerDay,'\n-----------------------------')
+    console.log(`\nGrupo ${group.name} - Jornada ${jornada + 1}:`);
+    dividingLine('-');
+    group.schedule[jornada].forEach(match => {
+        console.log(`${match.localTeam} ${match.localGoals} - ${match.awayGoals} ${match.awayTeam}`)
+    });
+
+    
+
+    console.table(leagueTeams.slice(indexSliceInit, indexSliceEnd).map(team =>{
+        return{
+            Equipo: team.name,
+            Puntos: team.points,
+            GolesAFavor: team.goalsFor,
+            GolesEnContra: team.goalsAgainst,
+            DiferenciaDeGoles: team.goalsFor - team.goalsAgainst
+
+        }
+    }));
 }
