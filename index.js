be605d491f117c60fc50f4c960ef3a895ab8606b
Georgina Torralba <georgina.torralba@gmail.com>
2021-01-05 04:59:01 +0100

    añado funcion showTitleWorldCupStageBegins

--- index.js
+++ index.js
@@ -6,25 +6,19 @@ import {showGroupsAndTeams, showTitleWorldCupStageBegins} from './consoleLog.js'
 
 const groups = [];
 export const leagueTeams = [];
+
 function start() {
 
     leagueTeamsCreator(leagueTeams);
-
-    //FASE DE GRUPS
-    
     creatorGroups(groups);
     scheduleMatches(groups);
     
     showGroupsAndTeams(groups);
 
-    //empiezan los partidos por jornada
-    //jugar partido
     showTitleWorldCupStageBegins('COMIENZA EL MUNDIAL');
     startGroupStage(groups);
+
     showTitleWorldCupStageBegins('COMIENZO DE LA FASE DE ELIMINATORIAS')
-
-    
-
 }
 
 start();
