3cbbaa5b305fe71d9230bdf8fc5a20a86ad33ad4
Georgina Torralba <georgina.torralba@gmail.com>
2021-01-05 05:04:07 +0100

    elimino imports que no se están utilizando

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-//exporto objecte team
-import {team, teamsNames} from './teams.js'
-import creatorGroups, { group } from './groups.js'
-import {leagueTeamsCreator, scheduleMatches, startGroupStage} from './league.js'
-import {showGroupsAndTeams, showTitleWorldCupStageBegins} from './consoleLog.js'
+
+import creatorGroups from './groups.js';
+import {leagueTeamsCreator, scheduleMatches, startGroupStage} from './league.js';
+import {showGroupsAndTeams, showTitleWorldCupStageBegins} from './consoleLog.js';
 
 const groups = [];
 export const leagueTeams = [];
