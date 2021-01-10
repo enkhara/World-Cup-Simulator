3e0ff2e3d8d2da9faa23b2ba621e61a37b60a9c1
Georgina Torralba <georgina.torralba@gmail.com>
2021-01-10 06:41:53 +0100

    Limpiando código de variables innecesarias

--- index.js
+++ index.js
@@ -4,19 +4,25 @@ import {leagueTeamsCreator, scheduleMatches, startGroupStage} from './league.js'
 import {showGroupsAndTeams, showTitleWorldCupStageBegins} from './consoleLog.js';
 import groupsCreator from './groups.js';
 
-const groups = [];
-export const leagueTeams = [];
-export const auxLeague = [];
+export const groups = [];
+//export const leagueTeams = [];
+//export const auxLeague = [];
 function start() {
 
     //leagueTeamsCreator(leagueTeams);
+    // groupsCreator(groups);
+    // scheduleMatches(groups);
+    
+    // showGroupsAndTeams(groups);
     groupsCreator(groups);
-    scheduleMatches(groups);
+    scheduleMatches();
     
     showGroupsAndTeams(groups);
 
     showTitleWorldCupStageBegins('COMIENZA EL MUNDIAL');
-    startGroupStage(groups);
+    //startGroupStage(groups);
+    startGroupStage();
+
 
     showTitleWorldCupStageBegins('COMIENZO DE LA FASE DE ELIMINATORIAS')
 }
@@ -25,4 +31,3 @@ start();
 
 
 
-console.log('auxleagye',auxLeague)
