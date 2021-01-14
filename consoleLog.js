a7c715400894284cbc0fc79b40a6493aee839e39
Georgina Torralba <georgina.torralba@gmail.com>
2021-01-14 01:57:17 +0100

    Implemento funciones showPlayOffRound, showMAtchesAndWinnerPerRound y showWinnerWorldChampionTeam

--- consoleLog.js
+++ consoleLog.js
@@ -1,6 +1,6 @@
 
 function dividingLine(symbol, repeat = 20){
-    console.log(symbol.repeat(repeat));
+    console.log(`${symbol.repeat(repeat)}`);
 }
 
 export function showGroupsAndTeams(groups){
@@ -52,15 +52,21 @@ export function showMatchesAndStandings(group, jornada){
 
         }
     }))
+}
 
-    // console.table(leagueTeams.map(team =>{
-    //     return{
-    //         Equipo: team.name,
-    //         Puntos: team.points,
-    //         GolesAFavor: team.goalsFor,
-    //         GolesEnContra: team.goalsAgainst,
-    //         DiferenciaDeGoles: team.goalsFor - team.goalsAgainst
+export function showPlayOffRound(title){
+    console.log(`\n${'='.repeat(5)} ${title} ${'='.repeat(5)}\n`)
+}
 
-    //     }
-    // }));
+export function showMatchesAndWinnerPerRound(matchesPlayOff, winnersPerRound){
+    for(let i = 0; i < matchesPlayOff.length; i ++){
+        console.log(`${matchesPlayOff[i].localTeam} ${matchesPlayOff[i].localGoals} - ${matchesPlayOff[i].awayGoals} ${matchesPlayOff[i].awayTeam} => ${winnersPerRound[i]}`)
+    }
+}
+
+export function showWinnerWorldChampionTeam(winner) {
+    console.log('\n')
+    dividingLine('=', 50 );
+    console.log(`¡${winner} campeón del mundo!`)
+    dividingLine('=', 50);
 }
