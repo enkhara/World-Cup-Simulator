5512dd2f4ba713e2674389f4e8d776060986a3b2
Georgina Torralba <georgina.torralba@gmail.com>
2021-01-14 02:01:25 +0100

    Elimino código comentado

--- groups.js
+++ groups.js
@@ -29,39 +29,6 @@ teamsNames.shuffle();
 const numberOfGroups = teamsNames.length / _NUMBEROFTEAMSPERGROUP_;
 
 
-//crea un grup amb 4 equips
-// function teamsGroup(index) {
-//     const indexSliceInit = index * _NUMBEROFTEAMSPERGROUP_;
-//     const indexSliceEnd = _NUMBEROFTEAMSPERGROUP_ + indexSliceInit;
-
-//     const group = teamsNames.slice(indexSliceInit, indexSliceEnd);
-
-//     return group;
-// }
-
-// function teamsGroup2(index) {
-//     const indexSliceInit = index * _NUMBEROFTEAMSPERGROUP_;
-//     const indexSliceEnd = _NUMBEROFTEAMSPERGROUP_ + indexSliceInit;
-
-//     const groupTeams = Object.assign({}, group);
-//     groupTeams.teams = teamsNames.slice(indexSliceInit, indexSliceEnd);
-//     //codigo ascii para las letras mayúsculas
-//     groupTeams.name = String.fromCharCode( index + 65 );
-//     auxLeague.push(groupTeams.name)
-//     return groupTeams;
-// }
-//omplim group.teams amb l'objecte team
-// function registerTeamsToTheGroup( teamsNames){
-    
-//     const groupConstructor = Object.assign({}, group); 
-//     teamsNames.forEach(teamName =>{
-//     const teamPerGroup = Object.assign({}, team);
-//     teamPerGroup.name = teamName;
-//     groupConstructor.teams.push(teamPerGroup)
-//     })
-//     return groupConstructor
-// }
-
 function teamsGroup(index) {
     const indexSliceInit = index * _NUMBEROFTEAMSPERGROUP_;
     const indexSliceEnd = _NUMBEROFTEAMSPERGROUP_ + indexSliceInit;
@@ -90,20 +57,8 @@ export function groupsCreator(groups){
     return groups;
 }
 
-
-// function playOffSchedulerScheme (groupNumber){
-//     const playOffSchedulerConstructor = [];
-//     const matchesPerSide = [];
-//     for (let i = 0; i < groupNumber; i++){
-//         const matchPerSide = Object.assign({}, match);
-        
-//     }
-// }
-
-
 export function playOffGroupBuilder (groups){
-    //const playOffScheduler = [];
-    //const matchesScheduleScheme = 
+    
     const playOffScheduler = matchScheme(groups.length);
     let indexGroupA = 0;
     let indexGroupB = groups.length / 2;
